761d35491938e1338464a2013d9be5c85c33a397
Sai Charan Kommana <saicharan.k3899@gmail.com>
2019-08-16 05:09:06 +0530

    Latest Version

---
+++ 7 Stones Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.IO;
+using System.Text;
+using System;
+
+[System.Serializable]
+public class Position
+{
+    public Vector3 stonesPosition;
+    public Vector3 ballPosition;
+}
+
+public class GameManager : MonoBehaviour
+{
+    public Position[] position;
+    public static int chances = 0;
+    public static bool gameRunning = false;
+    public GameObject stones;
+    public GameObject ball;
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
+    public GameObject panel;
+
+    private int previousScore = 0;
+    private static float timer = 0f;
+    private List<string[]> rowData = new List<string[]>();
+
+    private void Start()
+    {
+        StartCoroutine(InstantiateGameObject(0.5f));
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + score;
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(timer);
+
+            AddRecord(timer);
+            StartCoroutine(SaveScore());
+        }
+
+        timer += Time.deltaTime;
+    }
+
+    public IEnumerator InstantiateGameObject(float waitTime)
+    {
+        if (chances < 7)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Instantiate(stones, position[0].stonesPosition, Quaternion.identity);
+            Instantiate(ball, position[0].ballPosition, Quaternion.identity);
+            gameRunning = true;
+        }
+        else
+        {
+            chances = 0;
+            StartCoroutine("LoadScene");
+        }
+    }
+
+    private IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(1.5f);
+        scoreText.gameObject.SetActive(false);
+        panel.SetActive(true);
+        panel.GetComponentInChildren<TextMeshProUGUI>().text = "Score: " + score;
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Menu");
+    }
+
+    public static void AddRecord(float timeStamp)
+    {
+        try
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(Application.dataPath + "/" + "TimeStamps.csv", true))
+            {
+                file.WriteLine(timeStamp);
+            }
+        }
+        catch(Exception ex)
+        {
+            throw new ApplicationException("This program did an oopsie: ", ex);
+        }
+    }
+
+    private IEnumerator SaveScore()
+    {
+        yield return new WaitForSeconds(1.25f);
+        int scoreToRecord = score - previousScore;
+        previousScore = score;
+        try
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(Application.dataPath + "/" + "Score.csv", true))
+            {
+                file.WriteLine(scoreToRecord.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new ApplicationException("This program did an oopsie: ", ex);
+        }
+    }
+}
---
+++ 7 Stones Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float force;
+    public float sidewardsVelocity;
+    public float minimumXSpawnCoordinate, maximumXSpawnCoordinate;
+    public AudioClip strikeClip;
+
+    private Rigidbody playerRigibody;
+    private bool shot = false;
+    private bool strike = false;
+    private bool collide = true;
+    private bool stones = false;
+
+    private void Start()
+    {
+        playerRigibody = GetComponent<Rigidbody>();
+        playerRigibody.velocity = new Vector3(sidewardsVelocity, 0f, 0f);
+    }
+
+    private void Update()
+    {
+        if (GameManager.gameRunning && !shot)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+                shot = true;
+                GameManager.chances++;
+                strike = true;
+                this.GetComponent<AudioSource>().Play();
+            }
+        }
+
+        if (shot)
+        {
+            playerRigibody.useGravity = true;
+            playerRigibody.AddForce(Vector3.forward * force);
+        }
+        else
+        {
+            if (transform.position.x < minimumXSpawnCoordinate || transform.position.x > maximumXSpawnCoordinate)
+            {
+                if (this.gameObject.transform.position.x > 1.1f && sidewardsVelocity > 0)
+                    sidewardsVelocity = -sidewardsVelocity;
+                else if (this.gameObject.transform.position.x < 1.1f && sidewardsVelocity < 0)
+                    sidewardsVelocity = -sidewardsVelocity;
+
+                playerRigibody.velocity = new Vector3(sidewardsVelocity, 0f, 0f);
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Stones")
+        {
+            this.GetComponent<AudioSource>().clip = strikeClip;
+            this.GetComponent<AudioSource>().Play();
+            if (!stones)
+            {
+                FindObjectOfType<GameManager>().score += 4;
+                stones = true;
+                Debug.Log("asdf");
+            }
+        }
+
+        if (strike)
+        {
+            DestroyGameObjects();
+
+            FindObjectOfType<GameManager>().StartCoroutine("InstantiateGameObject", 2.5f);
+            strike = false;
+            GameManager.gameRunning = false;
+        }
+    }
+
+    private void DestroyGameObjects()
+    {
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Stones"))
+        {
+            Destroy(g, 2f);
+        }
+        Destroy(this.gameObject, 2f);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if((other.gameObject.name == "sideR" || other.gameObject.name == "sideL") && collide == true)
+            FindObjectOfType<GameManager>().score += 2;
+       
+        if (other.gameObject.name == "centre" && collide == true)
+            FindObjectOfType<GameManager>().score += 6;
+        collide = false;
+
+        Debug.Log("1234");
+    }
+}
